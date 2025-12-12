ca3d4c58ce3a49881d7c351c3d6ed45e6a382c88
Akintayo74 <akinnibosunakintayo74@gmail.com>
2025-12-12 17:36:36 +0100

    Remove prettier from template files

--- src/index.js
+++ src/index.js
@@ -108,7 +108,7 @@ mkDirPromise(componentDir)
   )
   .then((template) =>
     // Format it using prettier, to ensure style consistency, and write to file.
-    writeFilePromise(filePath, prettify(template))
+    writeFilePromise(filePath, template)
   )
   .then((template) => {
     logItemCompletion('Component built and saved to disk.');
