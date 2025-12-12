67bcdad0c9711b1ef0f971c2fe850ba8a6867a19
Akintayo74 <akinnibosunakintayo74@gmail.com>
2025-12-12 17:21:35 +0100

    Add comment to templates to force multi-line structure

--- src/templates/js.js
+++ src/templates/js.js
@@ -4,7 +4,7 @@ import styles from './COMPONENT_NAME.module.css';
 function COMPONENT_NAME() {
   return (
     <div className={styles.container}>
-      
+      {/* Component Code */}
     </div>
   )
 }
